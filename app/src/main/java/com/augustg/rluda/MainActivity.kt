bc024747eab10b6ffa7166de4a81cf612e6adb99
August Gruneisen <a.gruneisen@gmail.com>
2020-07-31 20:33:52 -0700

    add custom shapes and button

--- app/src/main/java/com/augustg/rluda/MainActivity.kt
+++ app/src/main/java/com/augustg/rluda/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.augustg.rluda
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var startButton: Button
+    private var startButtonFadeInDurationMillis: Long = 50
+    private var startButtonFadeOutDurationMillis: Long = 400
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.startButton)
+
+        startButton.setOnClickListener {
+            Toast.makeText(this, "Toast", Toast.LENGTH_SHORT).show()
+        }
+
+        fadeIn(startButton)
     }
+
+    private fun fadeIn(view: View) {
+        view.apply {
+            alpha = 0f
+            animate()
+                .alpha(1f)
+                .setStartDelay(0)
+                .setDuration(startButtonFadeInDurationMillis)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        fadeOut(view)
+                    }
+                })
+        }
+    }
+
+    private fun fadeOut(view: View) {
+        view.apply {
+            animate()
+                .alpha(0f)
+                .setStartDelay(3000)
+                .setDuration(startButtonFadeOutDurationMillis)
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        fadeIn(view)
+                    }
+                })
+        }
+    }
+
 }
