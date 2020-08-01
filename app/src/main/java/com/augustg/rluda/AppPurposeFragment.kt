dd32d96d75429753402ee5f663531b44b0ec6cc7
August Gruneisen <a.gruneisen@gmail.com>
2020-07-31 23:45:38 -0700

    extract animation logic

--- app/src/main/java/com/augustg/rluda/AppPurposeFragment.kt
+++ app/src/main/java/com/augustg/rluda/AppPurposeFragment.kt
@@ -1,23 +1,17 @@
 package com.augustg.rluda
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.augustg.rluda.databinding.FragmentAppPurposeBinding
+import com.augustg.rluda.util.pulse
 
-class AppPurposeFragment: Fragment() {
+class AppPurposeFragment : Fragment() {
 
     private lateinit var binding: FragmentAppPurposeBinding
-
-    private lateinit var startButton: Button
-    private var startButtonFadeInDurationMillis: Long = 50
-    private var startButtonFadeOutDurationMillis: Long = 400
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,41 +25,15 @@ class AppPurposeFragment: Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        startButton = binding.startButton
-
-        startButton.setOnClickListener {
+        binding.startButton.setOnClickListener {
             Toast.makeText(requireContext(), "Toast", Toast.LENGTH_SHORT).show()
         }
 
-        fadeIn(startButton)
-    }
-
-    private fun fadeIn(view: View) {
-        view.apply {
-            alpha = 0f
-            animate()
-                .alpha(1f)
-                .setStartDelay(0)
-                .setDuration(startButtonFadeInDurationMillis)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        fadeOut(view)
-                    }
-                })
-        }
-    }
-
-    private fun fadeOut(view: View) {
-        view.apply {
-            animate()
-                .alpha(0f)
-                .setStartDelay(3000)
-                .setDuration(startButtonFadeOutDurationMillis)
-                .setListener(object : AnimatorListenerAdapter() {
-                    override fun onAnimationEnd(animation: Animator) {
-                        fadeIn(view)
-                    }
-                })
-        }
+        binding.startButton.pulse(
+            fadeInDuration = 50,
+            fadeOutDuration = 400,
+            delayBetween = 2500,
+            initialDelay = 2000
+        )
     }
 }
---
+++ app/src/main/java/com/augustg/rluda/util/Animations.kt
@@ -0,0 +1,86 @@
+package com.augustg.rluda.util
+
+/**
+ * @author  August Gruneisen
+ * @since   2020-07-31
+ */
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.view.View
+
+/**
+ * Fades in a View
+ * View should already have alpha=0
+ *
+ * @param duration (ms)
+ * @param delayBefore (ms)
+ * @param onAnimationEnd (optional)
+ */
+fun View.fadeIn(
+    duration: Long,
+    delayBefore: Long,
+    onAnimationEnd: (() -> Unit) = {/* no-op */ }
+) {
+    this.apply {
+        animate()
+            .alpha(1f)
+            .setStartDelay(delayBefore)
+            .setDuration(duration)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    onAnimationEnd.invoke()
+                }
+            })
+    }
+}
+
+/**
+ * Fades out a View
+ * View should already have alpha=1
+ *
+ * @param duration (ms)
+ * @param delayBefore (ms)
+ * @param onAnimationEnd (optional)
+ */
+fun View.fadeOut(
+    duration: Long,
+    delayBefore: Long,
+    onAnimationEnd: (() -> Unit) = {/* no-op */ }
+) {
+    this.apply {
+        animate()
+            .alpha(0f)
+            .setStartDelay(delayBefore)
+            .setDuration(duration)
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator) {
+                    onAnimationEnd.invoke()
+                }
+            })
+    }
+}
+
+/**
+ * Pulses a View after some optional delay
+ * View should already have alpha=0
+ *
+ * @param fadeInDuration (ms)
+ * @param fadeOutDuration (ms)
+ * @param delayBetween (ms)
+ * @param initialDelay (optional)
+ */
+fun View.pulse(
+    fadeInDuration: Long,
+    fadeOutDuration: Long,
+    delayBetween: Long,
+    initialDelay: Long = 0
+) {
+    this.apply {
+        fadeIn(fadeInDuration, initialDelay) {
+            pulse(fadeInDuration, fadeOutDuration, delayBetween)
+            fadeOut(fadeOutDuration, delayBetween) {
+                this.pulse(fadeInDuration, fadeOutDuration, delayBetween)
+            }
+        }
+    }
+}
