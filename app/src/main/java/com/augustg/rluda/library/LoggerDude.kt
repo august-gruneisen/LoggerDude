ac5ae73e376373ed66f6ec748997ca0819da47ce
August Gruneisen <a.gruneisen@gmail.com>
2020-08-02 15:21:19 -0700

    add API methods

--- app/src/main/java/com/augustg/rluda/library/LoggerDude.kt
+++ app/src/main/java/com/augustg/rluda/library/LoggerDude.kt
@@ -1,6 +1,7 @@
 package com.augustg.rluda.library
 
 import android.content.Context
+import androidx.lifecycle.LiveData
 import com.augustg.rluda.library.storage.LogDatabase
 import com.augustg.rluda.library.storage.StorageManager
 
@@ -16,18 +17,59 @@ object LoggerDude {
     private lateinit var storageManager: StorageManager
 
     /**
-     * Initializes Logger Dude by creating a [StorageManager] to access local storage
+     * Initializes object by creating a [StorageManager] to access local storage
      *
      * @param context
      */
     fun initialize(context: Context) {
-        storageManager = StorageManager(
-            LogDatabase.getInstance(context).logDao()
-        )
+        storageManager = StorageManager(LogDatabase.getInstance(context).logDao())
         initialized = true
     }
 
-    fun method() {
+    /**
+     * Clears all stored Logs
+     *
+     * @throws Exception if used before initialization
+     */
+    fun clear() {
         if (!initialized) throw Exception("LoggerDude must be initialized before use")
+
+        storageManager.clearLogs()
+    }
+
+    /**
+     * Stores a Log message
+     *
+     * @param message
+     * @throws Exception if used before initialization
+     */
+    fun log(message: String) {
+        if (!initialized) throw Exception("LoggerDude must be initialized before use")
+
+        storageManager.storeLog(message)
+    }
+
+    /**
+     * Does something with the stored Logs
+     *
+     * @param doThis task to perform
+     * @throws Exception if used before initialization
+     */
+    fun withLogs(doThis: () -> Unit) {
+        if (!initialized) throw Exception("LoggerDude must be initialized before use")
+
+        storageManager.pullLogs { doThis.invoke() }
+    }
+
+    /**
+     * Gets Logs as a LiveData stream
+     *
+     * @return LiveData list of Logs
+     * @throws Exception if used before initialization
+     */
+    fun live(): LiveData<List<Log>> {
+        if (!initialized) throw Exception("LoggerDude must be initialized before use")
+
+        return storageManager.observeLogs()
     }
 }
