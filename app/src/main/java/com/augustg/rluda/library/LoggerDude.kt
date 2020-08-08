4dfac164515707a10ce295805d8b1ad776dc43ed
August Gruneisen <a.gruneisen@gmail.com>
2020-08-08 10:49:39 -0700

    implement outbound logging

--- app/src/main/java/com/augustg/rluda/library/LoggerDude.kt
+++ app/src/main/java/com/augustg/rluda/library/LoggerDude.kt
@@ -3,11 +3,13 @@ package com.augustg.rluda.library
 import android.content.Context
 import androidx.lifecycle.LiveData
 import com.augustg.rluda.library.logs.FormattedLog
+import com.augustg.rluda.library.outbound.NetworkCallback
+import com.augustg.rluda.library.outbound.NetworkManager
 import com.augustg.rluda.library.storage.LogDatabase
 import com.augustg.rluda.library.storage.StorageManager
 
 /**
- * Logger Dude API
+ * LoggerDude API
  * Initialize with a Context before using
  *
  * @throws Exception if used before calling [initialize]
@@ -17,6 +19,8 @@ object LoggerDude {
     private var initialized = false
 
     private lateinit var storageManager: StorageManager
+
+    private lateinit var networkManager: NetworkManager
 
     /**
      * Verifies that LoggerDude has been initialized
@@ -31,9 +35,11 @@ object LoggerDude {
      * Initializes object by creating a [StorageManager] to access local storage
      *
      * @param context
+     * @param endpoint (optional) specify an API to dump Logs
      */
-    fun initialize(context: Context) {
+    fun initialize(context: Context, endpoint: String) {
         storageManager = StorageManager(LogDatabase.getInstance(context).logDao())
+        networkManager = NetworkManager(endpoint)
         initialized = true
     }
 
@@ -56,16 +62,6 @@ object LoggerDude {
     }
 
     /**
-     * Does something with the stored Logs
-     *
-     * @param doThis task to perform
-     */
-    fun withLogs(doThis: (logs: List<FormattedLog>) -> Unit) {
-        verifyInitialization()
-        storageManager.pullLogs { doThis(it) }
-    }
-
-    /**
      * Gets Logs as a LiveData stream
      *
      * @return LiveData list of Logs
@@ -73,5 +69,26 @@ object LoggerDude {
     fun live(): LiveData<List<FormattedLog>> {
         verifyInitialization()
         return storageManager.observeLogs()
+    }
+
+    /**
+     * Does something with the stored Logs
+     *
+     * @param doThis task to perform
+     */
+    private fun withLogs(doThis: (logs: List<FormattedLog>) -> Unit) {
+        verifyInitialization()
+        storageManager.pullLogs { doThis(it) }
+    }
+
+    /**
+     * Sends the stored Logs to a remote server
+     *
+     * @param callback (optional) [NetworkCallback]
+     */
+    fun dump(callback: NetworkCallback? = null) {
+        withLogs {
+            networkManager.sendLogs(it, callback)
+        }
     }
 }
