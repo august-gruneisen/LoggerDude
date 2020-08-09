cd7bc136e359b9f89f837df31d9a39140b31542e
August Gruneisen <a.gruneisen@gmail.com>
2020-08-08 17:07:24 -0700

    remove capitalized folder

--- LoggerDude/src/main/java/com/augustg/loggerdude/outbound/NetworkCallback.kt
+++
@@ -1,9 +0,0 @@
-package com.augustg.loggerdude.outbound
-
-/**
- * Interface for handling the network response of outgoing Logs
- */
-interface NetworkCallback {
-    fun onSucceededSendingLogs()
-    fun onFailedSendingLogs()
-}
